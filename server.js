9f919565479177de436a61c3a339de64c35bf3cd
quirkles <al.quirk@gmail.com>
2020-04-19 11:32:23 -0400

    Change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080;
 
 app.use('/', express.static(path.join(__dirname, 'dist')))
 
